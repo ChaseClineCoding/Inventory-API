9f9c4e3d2ac80ba05332aa185fe7b3a71335515c
Chase (Ryan Cline) <ryancline88@yahoo.com>
2018-04-16 23:12:53 -0600

    created product data type

---
+++ src/main/java/com/chaseclinecoding/inventory/Product.java
@@ -0,0 +1,26 @@
+package com.chaseclinecoding.inventory;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class Product {
+
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    private Product() {}
+
+    public Product(String name, String description, BigDecimal price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
